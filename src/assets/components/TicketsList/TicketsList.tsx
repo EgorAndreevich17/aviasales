a1cf0b868dba40e7089b26f94ff7afdd21225282
Egor Andreevich <theonlyegor1709@gmail.com>
2024-12-20 01:37:08 +0300

    changed logic of filters

--- src/assets/components/TicketsList/TicketsList.tsx
+++ src/assets/components/TicketsList/TicketsList.tsx
@@ -5,9 +5,13 @@ import { RootState } from '../../../store'
 import TicketBox from '../TicketBox/TicketBox'
 
 export default function TicketsList() {
+  const filters = useSelector((state: RootState) => state.filters)
   const tickets = useSelector((state: RootState) => state.tickets.filteredTickets)
   const loading = useSelector((state: RootState) => state.tickets.loading)
   const error = useSelector((state: RootState) => state.tickets.error)
+
+  // Проверяем, активен ли хотя бы один фильтр
+  const isAnyFilterActive = filters.some((filter) => filter.isActive)
 
   if (loading) return <Spin />
 
@@ -15,8 +19,8 @@ export default function TicketsList() {
 
   return (
     <div>
-      {tickets.length === 0 ? (
-        <Empty description="Informational Notes" />
+      {!isAnyFilterActive || tickets.length === 0 ? (
+        <Empty description="Рейсов, подходящих под заданные фильтры, не найдено" />
       ) : (
         tickets.map((ticket, index) => <TicketBox key={index} ticket={ticket} />)
       )}
